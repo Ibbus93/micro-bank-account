97fa266645300f4c9493faa1d63f8caf07b4ee05
Federico Ibba <federicoibba@easy-network.it>
2020-02-05 10:54:28 +0100

    Updates

---
+++ precache-manifest.abd5350a14929b2ef032a26857251d87.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb736598479b2a39b857",
+    "url": "/micro-bank-account/static/css/main.8f537094.css"
+  },
+  {
+    "revision": "fb736598479b2a39b857",
+    "url": "/micro-bank-account/static/js/main.8b97eaf4.js"
+  },
+  {
+    "revision": "63b50fb5db07f8ec64ba94dbd1d18a53",
+    "url": "/micro-bank-account/index.html"
+  }
+];
