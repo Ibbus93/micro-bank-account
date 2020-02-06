190aba7aa5689830af48a5bb2b0f147765de7b1b
Federico Ibba <federicoibba@easy-network.it>
2020-02-06 10:02:04 +0100

    Updates

---
+++ precache-manifest.0f197db9167a156c6ab880c4072cf944.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "891c2b1be9085cba33f9",
+    "url": "/micro-bank-account/static/css/main.8f537094.css"
+  },
+  {
+    "revision": "891c2b1be9085cba33f9",
+    "url": "/micro-bank-account/static/js/main.6b0c75fb.js"
+  },
+  {
+    "revision": "65e39daa2c2cd1a7e329485e8b29f074",
+    "url": "/micro-bank-account/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/micro-bank-account/precache-manifest.e4e39574fd548c73d2630aa44663c267.js"
+  "/micro-bank-account/precache-manifest.0f197db9167a156c6ab880c4072cf944.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/micro-bank-account/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
