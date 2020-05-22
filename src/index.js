367372de2928570eeae82d2858e191c3a14ac943
Federico Ibba <federicoibba@easy-network.it>
2020-05-22 17:27:40 +0200

    first commit on new branch

--- src/index.js
+++ src/index.js
@@ -4,13 +4,7 @@ import ReactDOM from 'react-dom';
 import Root from './Root';
 import './index.css';
 
-window.renderBankAccount = (containerId, history, handler, auth) => {
-    ReactDOM.render(
-        <Root history={history} auth={auth} />,
-        document.getElementById(containerId)
-    );
-};
-
-window.unmountBankAccount = (containerId) => {
-    ReactDOM.unmountComponentAtNode(document.getElementById(containerId))
-};
+ReactDOM.render(
+  <Root history={history} auth={auth} />,
+  document.getElementById(containerId),
+);
